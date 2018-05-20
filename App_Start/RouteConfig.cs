ea550b98d6498733061debf3543a095d220987c4
Oleg Tarusov <ot88899888@gmail.com>
2018-05-20 16:31:34 +0300

    15. Routing

--- App_Start/RouteConfig.cs
+++ App_Start/RouteConfig.cs
@@ -13,10 +13,22 @@ namespace HelloApplication
 		{
 			routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            routes.MapRoute(
+                name: "GuestBook",
+                url: "page/{page}",
+                defaults: new { controller = "GuestBook", action = "Index" }
+            );
+
+            routes.MapRoute(
+                name: "Paging",
+                url: "{controller}/page/{page}",
+                defaults: new { action = "Index" }
+            );
+
 			routes.MapRoute(
 				name: "Default",
 				url: "{controller}/{action}/{id}",
-				defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+				defaults: new { controller = "GuestBook", action = "Index", id = UrlParameter.Optional }
 			);
 		}
 	}
--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -449,7 +449,7 @@ namespace HelloApplication.Controllers
             {
                 return Redirect(returnUrl);
             }
-            return RedirectToAction("Index", "Home");
+            return RedirectToAction("Index", "GuestBook");
         }
 
         internal class ChallengeResult : HttpUnauthorizedResult
